4beb45736aae1bc858377fc2de75e0a7807c2792
Phisanu Phoca <phisanuphoca@gmail.com>
2025-01-06 16:29:08 +0700

    fix : delete assets path

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	basePath: "/about-me",
-	assetPrefix: "/about-me/",
 };
 
 export default nextConfig;
