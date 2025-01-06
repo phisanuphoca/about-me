5961e765d203f13612f0611dfc8b31de23a5f634
Phisanu Phoca <phisanuphoca@gmail.com>
2025-01-06 16:36:00 +0700

    fix : update config

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "export",
 	reactStrictMode: true,
 	basePath: "/about-me",
+	assetPrefix: "/about-me/",
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
